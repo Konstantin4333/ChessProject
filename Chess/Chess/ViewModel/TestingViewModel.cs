e1155f87ae38dcd3d751f3740e08b282de865dcb
GeorgiYanushev <georgi_qnushev@abv.bg>
2022-08-15 13:14:49 +0300

    new grid added

---
+++ Chess/Chess/ViewModel/TestingViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess.ViewModel
+{
+    public class TestingViewModel
+    {
+        public int TestingRow { get; set; }
+        public int TestingColumn { get; set; }
+        
+       public TestingViewModel()
+        {
+            TestingRow = 2;
+            TestingColumn = 3 ;
+        }
+    }
+    
+}
+  
