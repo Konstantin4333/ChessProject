ba9a0fc0190655aceb07486af19577a3e76f7a5f
konstantin4333 <kmdimitrov13@gmail.com>
2022-08-29 10:43:58 +0300

    .

--- Chess/Chess/ViewModel/RowsAndColumnsViewModel.cs
+++ Chess/Chess/ViewModel/RowsAndColumnsViewModel.cs
@@ -1,13 +1,17 @@
 ﻿using Chess.Models;
+using Chess.View;
+using Prism.Commands;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace Chess.ViewModel
@@ -24,6 +28,7 @@ namespace Chess.ViewModel
         private Square _prevSquare;
         private List<Square> _availableMoves;
         private bool round = true;
+        private bool _IsEven;
 
         public void KingChecker(ObservableCollection<Square> squares, List<Square> AvailableMoves, Piece King)
         {
@@ -135,13 +140,13 @@ namespace Chess.ViewModel
                 _availableMoves = new List<Square>();
                 if (SPiece != null && SPiece.White && round == true)
                 {
-                    round = false;
+                    
                     _availableMoves = SPiece.SelectPath(Squares, PrevSquare);
                     if (SPiece.GetType() == typeof(King)) { KingChecker(Squares, _availableMoves, SPiece); }
                 }
                 if (SPiece != null && !SPiece.White && round == false)
                 {
-                    round = true;
+                    
                     _availableMoves = SPiece.SelectPath(Squares, PrevSquare);
                     if (SPiece.GetType() == typeof(King)) { KingChecker(Squares, _availableMoves, SPiece); }
                 }
@@ -152,6 +157,7 @@ namespace Chess.ViewModel
             {
                 if (_availableMoves.Contains(SelectedSquare))
                 {
+                    round = !round;
                     SelectedSquare.Piece = SPiece;
                     PrevSquare.Piece = null;
                 }
@@ -219,16 +225,201 @@ namespace Chess.ViewModel
             }
         }
 
+        public bool IsEven
+        {
+            get { return _IsEven; }
+            set
+            {
+                _IsEven = value;
+                OnPropertyChanged(nameof(IsEven));
+            }
+        }
+        public void CheckSquareEvent(Square[] sq)
+        {
+
+            // Squares = new Square[64];
+            for (int i = 0; i < 64; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sq[i].IsEven = true;
+
+                }
+
+            }
+            Console.WriteLine();
+
+        }
+
+
+
+        //--------------------------------------------
+
+
+
+        #region Border
+        private DelegateCommand _closeApp;
+        private DelegateCommand _minimize;
+
+        private DelegateCommand _resize;
+        private DelegateCommand _windowTest;
+        private DelegateCommand _resetApp;
+        private DelegateCommand _deathZone;
+
+
+
+
+        bool isOpened = false;
+
+
+
+
+
+        public ICommand CloseApp
+        {
+            get
+            {
+                return _closeApp ?? (_closeApp = new DelegateCommand(() =>
+                {
+                    App.Current.Shutdown();
+                }));
+            }
+        }
+
+
+        public ICommand ResetApp
+        {
+            get
+            {
+                return _resetApp ?? (_resetApp = new DelegateCommand(() =>
+                {
+                    var currentExecutablePath = Process.GetCurrentProcess().MainModule.FileName;
+                    Process.Start(currentExecutablePath);
+                    App.Current.Shutdown();
+
+                }));
+            }
+        }
+
+        public ICommand Minimize
+        {
+            get
+            {
+                return _minimize ?? (_minimize = new DelegateCommand(() =>
+                {
+                    App.Current.Windows[0].WindowState = WindowState.Minimized;
+                }));
+            }
+        }
+
+
+        public ICommand WindowTest
+        {
+            get
+            {
+                return _windowTest ?? (_windowTest = new DelegateCommand(() =>
+                {
+                    //*App.Current.Windows[0].WindowState = WindowState.Minimized;*//*
+
+
+                    UserControls.Add(GetUserControlInstance("DeathZone"));
+
+
+                }));
+            }
+        }
+
+        public ICommand DeathZonecmd
+        {
+            get
+            {
+                return _deathZone ?? (_deathZone = new DelegateCommand(() =>
+                {
+                    UserControl userControl = new DeathZonePieces();
+                    UserControls.Add(userControl);
+                    userControl.BringIntoView();
+
+                }));
+            }
+        }
+
+
+
+        public ICommand Resize
+        {
+            get
+            {
+                return _resize ?? (_resize = new DelegateCommand(() =>
+                {
+                    if (App.Current.Windows[0].WindowState == WindowState.Normal)
+                    {
+
+                        App.Current.Windows[0].WindowState = WindowState.Maximized;
+                        Button button = new Button();
+                        // Visibility.Visible = false;
+
+
+                    }
+                    else
+                    {
+                        App.Current.Windows[0].WindowState = WindowState.Normal;
+
+                    }
+
+                }));
+            }
+        }
+
+        private Visibility _MessageVisibilty;
+        public Visibility MessageVisibilty
+        {
+            get { return _MessageVisibilty; }
+            set { SetProperty(ref _MessageVisibilty, value); }
+        }
+
+        private void SetProperty(ref Visibility messageVisibilty, Visibility value)
+        {
+            MessageVisibilty = Visibility.Hidden;
+        }
+
+        public ObservableCollection<FrameworkElement> UserControls { get; set; } = new ObservableCollection<FrameworkElement>();
+        public void Execute(object? parameter)
+        {
+            // UserControls.Clear();
+            switch (parameter?.ToString())
+            {
+                case "DeathZone":
+                    UserControls.Add(GetUserControlInstance("DeathZone"));
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private FrameworkElement GetUserControlInstance(string v)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
 
 
 
         public RowsAndColumnsViewModel()
         {
+
+
+
             Board = new Board();
             Squares = new ObservableCollection<Square>(Board.Squares);
+            IsEven = new bool();
+
+
 
         }
 
     }
 
 }
+
+
