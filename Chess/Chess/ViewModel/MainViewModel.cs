35dee926a7e43c541cda8059d6fcd30c57f6345e
konstantin4333 <kmdimitrov13@gmail.com>
2022-09-01 14:23:28 +0300

    Round View, Rounds, View edits, ViewModel edits

--- Chess/Chess/ViewModel/MainViewModel.cs
+++ Chess/Chess/ViewModel/MainViewModel.cs
@@ -5,15 +5,6 @@ namespace Chess.ViewModel
     public class MainViewModel : BaseViewModel
     {
         
-        private static void transparency()
-        {
-            if (App.Current.Windows[0].WindowState == WindowState.Normal)
-            {
-                App.Current.Windows[0].AllowsTransparency = true;
-            }else if (App.Current.Windows[0].WindowState == WindowState.Maximized)
-            {
-                App.Current.Windows[0].AllowsTransparency = false;
-            }
-        }
+        
     }
 }
